81e774e1255aabdfa5ede65321566e3c63406b2c
David Morton <55235981+davidmorton0@users.noreply.github.com>
2022-01-28 22:16:01 +0000

    yarn build

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,6 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-import * as bootstrap from "bootstrap"
+import "@hotwired/turbo-rails"  
+import "./controllers"  
+import * as bootstrap from "bootstrap"  
   
 let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))  
 let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {  
