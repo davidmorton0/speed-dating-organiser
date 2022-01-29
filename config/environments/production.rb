6c81a411d59108b745740b5556bd883a15c9a897
David Morton <55235981+davidmorton0@users.noreply.github.com>
2022-01-29 18:51:28 +0000

    move sendgrid apikey to credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
     :port => '587',
     :authentication => :plain,
     :user_name => 'apikey',
-    :password => ENV['SENDGRID_API_KEY'],
+    :password => Rails.application.credentials.sendgrid.apikey,
     :domain => 'heroku.com',
     :enable_starttls_auto => true
   }
